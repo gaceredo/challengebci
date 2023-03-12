67efe3bac0247957aec0730f432355b015123fd8
gaceredo <40033476+gaceredo@users.noreply.github.com>
2023-03-12 10:10:46 -0500

    Configure localizable

---
+++ challengebci/Core/BaseClasses/BaseViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BaseViewController.swift
+//  challengebci
+//
+//  Created by Gustavo Aceredo on 12/3/23.
+//
+
+import UIKit
+
+/// Base view controller
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
